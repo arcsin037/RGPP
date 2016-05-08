0bd623c21fab062932fd3e1e3a0f1fcd85869aa3
arcsin <arcsin037@gmail.com>
2016-05-08 13:55:08 +0900

    Coreのフォルダ構成の変更

--- src/js/Core/index.js
+++ src/js/Core/index.js
@@ -1,5 +1,5 @@
-import Config from './Common/Config.json'
-import Const from './Common/Const.json'
+import Config from './Config'
+import Const from './Const/Const.json'
 import MW from 'MW'
 import System from 'System'
 import User from 'User'
@@ -11,58 +11,5 @@ RGPP.Config = Config || {}
 RGPP.System = System || {}
 RGPP.MW = MW || {}
 RGPP.User = User || {}
-
-/**
- * Set configuration parameter.
- *
- * @method setConfigParam
- * @param configName {String} Name of configuration parameter
- * @param configValue {*} Value of configuration parameter
- */
-const setConfigParam = (configName, configValue) => {
-    if (isInValidConfigName(configName)) {
-        return
-    }
-
-    if (RGPP && RGPP.Config) {
-        RGPP.Config[configName] = configValue
-    }
-}
-
-/**
- * Return configuration parameter.
- *
- * @method getConfigParam
- * @param configName {String} Name of configuration parameter
- * @return Configuration parameter / If the configuration name is invalid, return undefined
- */
-const getConfigParam = (configName) => {
-    if (isInValidConfigName(configName)) {
-        return
-    }
-    if (RGPP && RGPP.Config) {
-        return RGPP.Config[configName]
-    }
-}
-
-/**
- * Whether the configuration name is invalid or not.
- *
- * @method isInValidConfigName
- * @param {string} configName Name of configuration parameter
- * @return Whether the configuration name is invalid or not.
- * @private
- */
-const isInValidConfigName = (configName) => {
-    if (!(typeof configName === 'string') ||
-        configName === 'setConfigParam' ||
-        configName === 'getConfigParam') {
-        return true
-    }
-    return false
-}
-
-RGPP.Config.setConfigParam = setConfigParam
-RGPP.Config.getConfigParam = getConfigParam
 
 export default RGPP
