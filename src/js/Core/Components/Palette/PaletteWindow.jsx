913e8baddb7a131f6aecf0ab488e8c147372f4b5
arcsin <arcsin037@gmail.com>
2016-05-15 22:45:17 +0900

    draggableに

--- src/js/Core/Components/Palette/PaletteWindow.jsx
+++ src/js/Core/Components/Palette/PaletteWindow.jsx
@@ -1,8 +1,7 @@
-import $ from 'jquery'
+import Draggable from 'react-draggable'
 import React from 'react'
 import SelectBox from 'Core/Components/Base/SelectBox.jsx'
 
-import 'jquery-ui'
 import saveIcon from 'Image/System/save.svg'
 
 const drawModeOptions = [
@@ -25,19 +24,20 @@ const drawModeOptions = [
 class PaletteWindow extends React.Component {
     constructor(props) {
         super(props)
-        const $mWindow = $('#palette-window')
 
-        $mWindow.dialog({autoOpen: true, width: 640, height: 500})
-        console.log('Dialog!!!!')
-        $mWindow.dialog('option', 'title', 'Palette')
-        $mWindow.dialog('open')
+        // const $mWindow = $('#palette-window')
+        //
+        // $mWindow.dialog({autoOpen: true, width: 640, height: 500})
+        // console.log('Dialog!!!!')
+        // $mWindow.dialog('option', 'title', 'Palette')
+        // $mWindow.dialog('open')
 
         const openFunc = () => {
 
             // var mapPanelList = RGPP.System.MapPanelList.getInstance()
             // if (!mapPanelList.isEventLayer()) {
-            $mWindow.dialog('option', 'title', 'Palette')
-            $mWindow.dialog('open')
+            // $mWindow.dialog('option', 'title', 'Palette')
+            // $mWindow.dialog('open')
             //     var pList = RGPP.System.PalettePanelList.getInstance()
             //     var currentPalettePanel = pList.currentPalettePanel()
             //     if (currentPalettePanel !== null) {
@@ -47,7 +47,7 @@ class PaletteWindow extends React.Component {
         }
 
         const closeFunc = () => {
-            $mWindow.dialog('close')
+            // $mWindow.dialog('close')
         }
 
         this.openFunc = openFunc
@@ -57,18 +57,20 @@ class PaletteWindow extends React.Component {
 
     render() {
         return (
-            <div id='palette-window' title='Palette'>
-                <div id='palette-window-menu'>
-                    <SelectBox options={drawModeOptions} defaultValue='Pen'/>
-                    <label>Tag value</label>
-                    <input id='tag-data-spinner'/>
+            <Draggable handle='div'>
+                <div id='palette-window' title='Palette'>
+                    <div id='palette-window-menu'>
+                        <SelectBox options={drawModeOptions} defaultValue='Pen'/>
+                        <label>Tag value</label>
+                        <input id='tag-data-spinner'/>
 
-                    <button id='save-tag-data' className='btn' title='Save Tag Data'>
-                        <img src={saveIcon}/>
-                    </button>
+                        <button id='save-tag-data' className='btn' title='Save Tag Data'>
+                            <img src={saveIcon}/>
+                        </button>
+                    </div>
+                    <div id='PalettePanelItemDiv'></div>
                 </div>
-                <div id='PalettePanelItemDiv'></div>
-            </div>
+            </Draggable>
         )
     }
 }
