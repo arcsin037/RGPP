4d82e0c00011d75b5f91c13ecade2c4e8fc4f077
arcsin <arcsin037@gmail.com>
2016-10-10 20:47:03 +0900

    RGPPにVectorを追加

--- src/js/RGPP/System/Utils/index.js
+++ src/js/RGPP/System/Utils/index.js
@@ -2,10 +2,12 @@ import BrowserUtil from './BrowserUtil'
 import NumberUtil from './NumberUtil'
 import Random from './Random'
 import TypeUtil from './TypeUtil'
+import Vector2 from './Vector2'
 
 export default {
     BrowserUtil,
     TypeUtil,
     Random,
-    NumberUtil
+    NumberUtil,
+    Vector2
 }
