e472a28142c348e4fdaf5d23c15351d77dd67e2e
arcsin037 <arcsin037@arcsin037noMacBook-Pro.local>
2016-12-04 14:32:54 +0900

    Mouse.jsの削除

--- src/js/Core/actions/Images/ImagesActions.test.js
+++ src/js/Core/actions/Images/ImagesActions.test.js
@@ -7,16 +7,16 @@ import {
 } from 'chai'
 
 describe('ImagesActions', () => {
-    it('should create an action to add an image', () => {
-        const image = {
-            name: 'hoge',
-            description: 'description',
-            url: 'url'
-        }
-        const expectedAction = {
-            type: types.ADD_IMAGE,
-            data: image
-        }
-        expect(actions.addImage(image)).to.deep.equal(expectedAction)
-    })
+  it('should create an action to add an image', () => {
+    const image = {
+      name: 'hoge',
+      description: 'description',
+      url: 'url'
+    }
+    const expectedAction = {
+      type: types.ADD_IMAGE,
+      data: image
+    }
+    expect(actions.addImage(image)).to.deep.equal(expectedAction)
+  })
 })
--- src/js/Core/actions/Input/Mouse.js
+++
@@ -1,5 +0,0 @@
-export const ON_MOUSE_MOVE = 'ON_MOUSE_MOVE'
-
-export const onmousemove = (e) => ({
-    type: ON_MOUSE_MOVE
-})
