dad541c9de8da0d2c89dc39926582f47941b3e82
arcsin037 <arcsin037@arcsin037noMacBook-Pro.local>
2016-12-04 15:04:56 +0900

    不要ファイル削除

--- src/js/Core/reducers/index.js
+++ src/js/Core/reducers/index.js
@@ -5,13 +5,9 @@ import {pluginReducers} from 'Plugin'
 import saveData from './saveData'
 import scenes from './scenes'
 import sounds from './sounds'
-import todos from './todos'
-import visibilityFilter from './visibilityFilter'
 
 const reducer = combineReducers({
   plugin: pluginReducers,
-  todos,
-  visibilityFilter,
   saveData,
   scenes,
   images,
